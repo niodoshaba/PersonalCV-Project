e802e93a4e2a2c3f4fbb8c6414bf73da64f1d6de
niodoshaba <niodoshaba@gmail.com>
2022-03-21 15:36:01 +0800

    feat: RWD++，動畫調整

--- js/main.js
+++ js/main.js
@@ -1,17 +1,37 @@
+
+
 $(function(){
-    
-    $(".header-border").animate({opacity:'1'},function(){
+
+    var w = $(window).width();
+    var h = $(window).height();
+
+ 
+    $(".header-border").animate({opacity:'1'},100,function(){
         $(".info").animate({right:'-70px'},1000);
         $(".info").animate({right:'-80px'},100);
         $(".info").animate({right:'-78px'},100,function(){
-            $(".skill-div").animate({opacity:'1'},function(){
-                $(".exp-div").animate({opacity:'1'},function(){
-                    $(".project-div").animate({opacity:'1'}),function(){
-                        $(".about-div").animate({opacity:'1'});
-                    };
+            $(".contact").animate({opacity:'1'},function(){
+                $(".skill-div").animate({opacity:'1'},function(){
+                    $(".exp-div").animate({opacity:'1'},function(){
+                        $(".project-div").animate({opacity:'1'}),function(){
+                            $(".about-div").animate({opacity:'1'});
+                        };
+                    });
                 });
-            });
+            })
         });    
     });
+    
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 100){
+            $(".to-top").fadeIn()
+        } else{
+            $(".to-top").fadeOut()
+        }
+    })
+    $(".to-top").click(function(){
+        $('html, body').animate({scrollTop: 0}, 200);
+    })
 
 })
